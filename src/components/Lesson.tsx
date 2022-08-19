c6a8bf866f8cb5718ab8994a2fa2a3ef0c6d4d7f
Kaio Pessoni <kaiopessoni@gmail.com>
2022-08-19 10:43:02 -0300

    Add video component and router

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -25,11 +26,11 @@ export const Lesson = ({
   )
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">
         {availableDateFromatted}
       </span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
         <header className="flex items-center justify-between">
 
           { isLessonAvailable ? (
@@ -44,7 +45,7 @@ export const Lesson = ({
             </span>
           )}
 
-          <span className="text-xs rounded px-2 py-[2px] text-white border border-green-300 font-bold">
+          <span className="text-xs rounded px-2 py-1 text-white border border-green-300 font-bold">
             {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
@@ -53,6 +54,6 @@ export const Lesson = ({
           {title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,146 @@
-export const Video = (): JSX.Element => {
+import { DefaultUi, Player, Youtube } from "@vime/react"
+import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react"
+
+import '@vime/core/themes/default.css'
+import { gql, useQuery } from "@apollo/client";
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson (where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+export const Video = ({ lessonSlug }: VideoProps): JSX.Element => {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: lessonSlug
+    }
+  })
+  
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1">
-      Video
+      {/* Lesson Player */}
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
+        </div>
+      </div>
+
+      {/* Lesson content */}
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            {/* Title and description */}
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              {data.lesson.description}
+            </p>
+            {/* Teacher details */}
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src="https://github.com/kaiopessoni.png"
+                alt="avatar"
+              />
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+              </div>
+            </div>
+          </div>
+          {/* Buttons */}
+          <div className="flex flex-col gap-4 ">
+            <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+              <DiscordLogo size={24} />
+              Comunidade do Discord
+            </a>
+            <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 transition-colors hover:text-gray-900">
+              <Lightning size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          {/* First card */}
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            {/* Download icon */}
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40}/>
+            </div>
+            {/* Card title and description */}
+            <div className="py-6 leading-relaxed">
+              <strong className="text-sm text-gray-200 mt-2">
+                Material Complementar
+              </strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            {/* Caret icon */}
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+
+          {/* Second card */}
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            {/* Download icon */}
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <Image size={40}/>
+            </div>
+            {/* Card title and description */}
+            <div className="py-6 leading-relaxed">
+              <strong className="text-sm text-gray-200 mt-2">
+              Wallpapers exclusivos
+              </strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+              </p>
+            </div>
+            {/* Caret icon */}
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
